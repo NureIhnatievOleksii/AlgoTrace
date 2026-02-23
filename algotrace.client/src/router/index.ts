9aa22848997c4459c8377947a4d9346c5530ed9d
AlexaChal <shurshurych2005@gmail.com>
2026-02-23 11:30:30 +0200

    Changes for report page

---
+++ algotrace.client/src/router/index.ts
@@ -0,0 +1,27 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../components/HomeView.vue';
+import AuthView from '../components/AuthView.vue';
+import AnalyzerPage from '../components/AnalyzerPage.vue';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/auth',
+      name: 'auth',
+      component: AuthView
+    },
+    {
+      path: '/analyzer',
+      name: 'analyzer',
+      component: AnalyzerPage 
+    }
+  ]
+});
+
+export default router;
