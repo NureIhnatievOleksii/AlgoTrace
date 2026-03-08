b1b228267ee751a5bfdafefbdb76b949c802caee
OleksiiIhnatiev <oleksii.ihnatiev28@gmail.com>
2026-03-08 02:26:40 +0200

    feat(api): add graph and metric comparison endpoints

--- AlgoTrace.Server/Algorithms/LevenshteinAlgorithm.cs
+++ AlgoTrace.Server/Algorithms/LevenshteinAlgorithm.cs
@@ -56,15 +56,21 @@ namespace AlgoTrace.Server.Algorithms
                 m = t.Length;
             int[,] d = new int[n + 1, m + 1];
             for (int i = 0; i <= n; d[i, 0] = i++)
-                ;
+            {
+            }
             for (int j = 0; j <= m; d[0, j] = j++)
-                ;
+            {
+            }
             for (int i = 1; i <= n; i++)
-            for (int j = 1; j <= m; j++)
-                d[i, j] = Math.Min(
-                    Math.Min(d[i - 1, j] + 1, d[i, j - 1] + 1),
-                    d[i - 1, j - 1] + (s[i - 1] == t[j - 1] ? 0 : 1)
-                );
+            {
+                for (int j = 1; j <= m; j++)
+                {
+                    d[i, j] = Math.Min(
+                        Math.Min(d[i - 1, j] + 1, d[i, j - 1] + 1),
+                        d[i - 1, j - 1] + (s[i - 1] == t[j - 1] ? 0 : 1)
+                    );
+                }
+            }
             return d[n, m];
         }
     }
